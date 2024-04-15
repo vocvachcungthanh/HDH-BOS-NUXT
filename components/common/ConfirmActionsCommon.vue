2968cbb7cc8020a528e99ff56e89fca8c717acaf
Thành Nguyễn Hữu <thanhnguyenhuu@Thanhs-MacBook-Pro.local>
2024-04-15 21:47:45 +0700

    fix: delete-restore

--- components/common/ConfirmActionsCommon.vue
+++ components/common/ConfirmActionsCommon.vue
@@ -1,17 +1,50 @@
 <template>
   <div class="flex items-center justify-center gap-3">
+    <a-tooltip v-if="!isText">
+      <template slot="title">
+        <span> Khôi phục </span>
+      </template>
+      <a-button
+        :size="size"
+        class="flex items-center text-blue-600 border-blue-600 hover:bg-blue-500 hover:text-white gap-1"
+        @click="handleRestore"
+      >
+        <a-icon type="sync" />
+        {{ getTextRestore }}
+      </a-button>
+    </a-tooltip>
     <a-button
+      v-else
       :size="size"
-      class="flex items-center text-blue-600 border-blue-600 hover:bg-blue-500 hover:text-white"
+      class="flex items-center text-blue-600 border-blue-600 hover:bg-blue-500 hover:text-white gap-1"
       @click="handleRestore"
-      ><a-icon type="sync" />Khôi phục</a-button
     >
+      <a-icon type="sync" />
+      {{ getTextRestore }}
+    </a-button>
+
+    <a-tooltip v-if="!isText">
+      <template slot="title">
+        <span> Xóa </span>
+      </template>
+      <a-button
+        :size="size"
+        class="flex items-center text-red-600 border-red-600 hover:bg-red-500 hover:text-white gap-1"
+        @click="handleDelete"
+      >
+        <a-icon type="sync" />
+        {{ getTextDelete }}
+      </a-button>
+    </a-tooltip>
     <a-button
+      v-else
       :size="size"
-      class="flex items-center text-red-600 border-red-600 hover:bg-red-500 hover:text-white"
+      class="flex items-center text-red-600 border-red-600 hover:bg-red-500 hover:text-white gap-1"
       @click="handleDelete"
-      ><a-icon type="delete" /> Xóa</a-button
     >
+      <a-icon type="sync" />
+      {{ getTextDelete }}
+    </a-button>
   </div>
 </template>
 
@@ -21,6 +54,21 @@ export default {
     size: {
       type: String,
       default: 'default',
+    },
+
+    isText: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  computed: {
+    getTextDelete() {
+      return this.isText ? 'Xóa' : ''
+    },
+
+    getTextRestore() {
+      return this.isText ? 'Khôi phục' : ''
     },
   },
 
