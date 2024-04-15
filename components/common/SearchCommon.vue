d8cec1092fcd76e9eecc449a5462e7fbbeb3d591
Thành Nguyễn Hữu <thanhnguyenhuu@Thanhs-MacBook-Pro.local>
2024-04-16 06:37:49 +0700

    feat: search-unit

---
+++ components/common/SearchCommon.vue
@@ -0,0 +1,65 @@
+<template>
+  <a-form
+    id="components-form-normal-search"
+    ref="formRef"
+    :form="form"
+    class="search-form"
+    @submit="onSearch"
+  >
+    <a-input-search
+      v-decorator="[
+        'name',
+        {
+          initialValue: key,
+          rules: [
+            {
+              required: true,
+              message: 'Nhập key tìm kiếm',
+            },
+          ],
+        },
+      ]"
+      :placeholder="placeholder"
+      :loading="loading"
+    />
+  </a-form>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: 'Tìm kiếm ...',
+    },
+
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      key: 'vào',
+    }
+  },
+
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: 'normal-search' })
+  },
+
+  methods: {
+    onSearch(e) {
+      e.preventDefault()
+
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log(values)
+          //   this.$emit('on-key-search', values)
+        }
+      })
+    },
+  },
+}
+</script>
---
+++ components/pages/HumanResources/Units/TableUnit/SearchTableUnit.vue
@@ -0,0 +1,19 @@
+<template>
+  <SearchCommon @on-key-search="handleSearch" />
+</template>
+
+<script>
+import SearchCommon from '~/components/common/SearchCommon.vue'
+
+export default {
+  components: {
+    SearchCommon,
+  },
+
+  methods: {
+    handleSearch(value) {
+      console.log(value)
+    },
+  },
+}
+</script>
